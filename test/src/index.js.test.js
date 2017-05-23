00740336804a755856e8c623b54dbc179a9ae387
Joakim J. Rapp <joakimrapp@hotmail.com>
2017-05-23 17:25:19 +0200

    wip

--- test/src/index.js.test.js
+++ test/src/index.js.test.js
@@ -1,14 +1,14 @@
-require( '../../../helpers/unit.js' )( ( finder ) => ( {
+require( '@jrapp/node-project-setup' ).testing.file( './test' )( ( finder ) => ( {
 	setup: require( '@jrapp/log-emitter' ).on( 'debug', () => {} )
 } ) )
 	.it( 'should find all files in absolute path matching a rule', ( assert, finder ) =>
-		finder( require( 'path' ).resolve( __dirname, '../../..' ), '*.js' )
+		finder( require( 'path' ).resolve( __dirname, '..' ), '*.js' )
 			.then( absolutepaths => absolutepaths.map( absolutepath => require( 'path' ).basename( absolutepath ) ) )
-	 		.then( filenames => assert.deepEqual( filenames, [ 'unit.js', 'index.js.test.js' ] ) ) )
+	 		.then( filenames => assert.deepEqual( filenames, [ 'index.js.test.js' ] ) ) )
 	.it( 'should find all files in relative path matching a rule', ( assert, finder ) =>
-		finder( '../../..', '*.js' )
+		finder( '..', '*.js' )
 			.then( absolutepaths => absolutepaths.map( absolutepath => require( 'path' ).basename( absolutepath ) ) )
-	 		.then( filenames => assert.deepEqual( filenames, [ 'unit.js', 'index.js.test.js' ] ) ) )
+	 		.then( filenames => assert.deepEqual( filenames, [ 'index.js.test.js' ] ) ) )
 	.it( 'should throw exception if absolute path does not exists', ( assert, finder ) => new Promise( resolve =>
 		finder( '/wefjofewjo/FewfewFUEWF/fwef', '*.js' ).catch( err => resolve() ) ) )
 .done();
